e4ce2755b6e8ae62ae5eb7ef69640073e3074389
Deividas <d.j.miliauskas@gmail.com>
2018-03-16 17:06:59 +0200

    fixed header bug, writing bug

--- s1612801.py
+++ s1612801.py
@@ -2,24 +2,39 @@ from datetime import datetime
 import sys
 
 
-def read_file(data, header):
+data = []
+results = []
+header = []
+
+
+
+def read_header():
     file = open("input01.csv", "r")
     header_string = next(file)
+    header = header_string.strip().split(',')
+    return header
 
-    header = header_string.split(',')
-    print(header)
+
+def read_file(data):
+    file = open("input01.csv", "r")
+    header_string = next(file)
     file_data = file.readlines()
     #print(file_data[0])
     for line in file_data:
-        array = line.split(',')
+
+        array = line.strip().split(',')
         data.append(array)
     file.close()
 
 
 def task_a(header):
+    temp = []
     for element in header:
         element = element[:1].upper() + element[1:]
-    print(header)
+        #print(element)
+        temp.append(element)
+    #print(temp)
+    return temp
 
 
 def task_b(data, results, column=2, sort_type="ends", string_part="INC"):
@@ -32,41 +47,39 @@ def task_b(data, results, column=2, sort_type="ends", string_part="INC"):
 
 
 def task_c(results, column=7, element_type="date", sort_type="ASC"):
-    print(len(results))
-    print(results[1][column-1])
-
+    #print(len(results))
+    #print(results[1][column-1])
     if element_type is "date":
         sorted(results, key=lambda x: datetime.strptime(x[column-1], "%Y-%m-%d"))
 
 
-def task_d(results, column1=8, column2=22):
+def task_d(results, header, column1=8, column2=22):
+    header[column1 - 1], header[column2 -1] = header[column2 - 1], header[column1 -1]
     for lines in results:
         lines[column1 - 1], lines[column2 - 1] = lines[column2 - 1], lines[column1 - 1]
-        #print(line)
+        #print(lines)
 
 
-def write_file(results):
+def write_file(results, header):
     file = open("output01_[1612801].txt", "w")
+    file.write('\t'.join(header))
+    file.write('\n')
     for line in results:
         file.write('\t'.join(line))
+        file.write('\n')
     #print(newline)
     file.close()
 
 
-def main():
-    data = []
-    results = []
-    header = []
-    paramE1 = "\t"
-    read_file(data, header)
-    task_a(header)
-    print(header)
-    task_b(data, results)
-    task_c(results)
-    task_d(results)
-    write_file(results)
-    #for rez_line in data:
-   #     print(rez_line)
+header = read_header()
+read_file(data)
+header = task_a(header)
+#print(header)
+task_b(data, results)
+task_c(results)
+#task_d(results, header, 8, 22)
+write_file(results, header)
+for rez_line in data:
+    print(rez_line)
 
 
-main()
